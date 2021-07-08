d42fd7a3765bf79f6d631983bef89f6e17f8be53
Nilay <54245048+dnilay@users.noreply.github.com>
2021-07-08 18:05:22 +0530

    Add files via upload

---
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -0,0 +1,29 @@
+package com.appsdeveloperblog.photoapp.api.users;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication
+
+public class PhotoAppApiUsersApplication {
+
+	@Autowired
+	Environment environment;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppApiUsersApplication.class, args);
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder()
+	{
+		return new BCryptPasswordEncoder();
+	}
+	
+	
+
+}
