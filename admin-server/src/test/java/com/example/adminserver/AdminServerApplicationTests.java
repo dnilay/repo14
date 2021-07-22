315e23d2ebab65fa520aa991f0f54ff01896fd5a
Nilay <54245048+dnilay@users.noreply.github.com>
2021-07-22 09:04:05 +0530

    Add files via upload

--- admin-server/src/test/java/com/example/adminserver/AdminServerApplicationTests.java
+++ admin-server/src/test/java/com/example/adminserver/AdminServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.adminserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaServerApplicationTests {
+class AdminServerApplicationTests {
 
     @Test
     void contextLoads() {
